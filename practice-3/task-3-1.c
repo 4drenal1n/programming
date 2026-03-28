903c32c88f48ebf61629952a68967d53f93bd58a
4drenal1n <lg.brutalelite@mail.ru>
2026-03-28 14:00:37 +0300

    Update task-3-1.c

--- practice-3/task-3-1.c
+++ practice-3/task-3-1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(void) {
+    double pi = 3.14159;
+    double end = 2 * pi;
+
+    for (double x = 0; x <= end + 0.0001; x += 0.1) {
+        printf("x = %.2lf, sin(x) = %.4lf\n", x, sin(x));
+    }
+
+    return 0;
+}
