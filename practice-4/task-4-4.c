6ec78e42fa777c55236241e6b0ac9f2459bf5fce
4drenal1n <lg.brutalelite@mail.ru>
2026-03-28 15:04:31 +0300

    Update task-4-4.c

--- practice-4/task-4-4.c
+++ practice-4/task-4-4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+// Функция обмена двух целых чисел через указатели
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter first number: ");
+    scanf("%d", &x);
+    printf("Enter second number: ");
+    scanf("%d", &y);
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap:  x = %d, y = %d\n", x, y);
+
+    return 0;
+}
