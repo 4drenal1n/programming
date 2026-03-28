5308347b4ab72242e6af96809ab4e658b4227123
4drenal1n <lg.brutalelite@mail.ru>
2026-03-28 14:54:17 +0300

    Update task-4-2.c

--- practice-4/task-4-2.c
+++ practice-4/task-4-2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+// Рекурсивная функция вычисления факториала
+long long factorial_rec(int n) {
+    if (n < 0) {
+        
+        return 0;
+    }
+    if (n == 0) {
+        return 1; 
+    }
+    return n * factorial_rec(n - 1); 
+}
+
+int main() {
+    int n;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+    printf("%d! = %lld\n", n, factorial_rec(n));
+    return 0;
+}
