77149bc60e69dbe2616a3d37978c902c8deae2bd
4drenal1n <lg.brutalelite@mail.ru>
2026-03-28 14:50:50 +0300

    Update task-4-1.c

--- practice-4/task-4-1.c
+++ practice-4/task-4-1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+
+long long factorial(int n) {
+    if (n < 0) {
+        return 0; // факториал отрицательного числа не определён
+    }
+    long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+    printf("%d! = %lld\n", n, factorial(n));
+    return 0;
+}
